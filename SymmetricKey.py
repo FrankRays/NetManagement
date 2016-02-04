9e52365f7b68ec0ec0023ca69145fc9f5b4d52be
hadarrabin <hadarrabin1@gmail.com>
2016-02-04 12:50:21 +0200

    Update SymmetricKey.py

--- SymmetricKey.py
+++ SymmetricKey.py
@@ -1,6 +1,10 @@
+from Crypto.Cipher import AES
 import base64
+import sys
 BS = 16
+
 pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+
 unpad = lambda s : s[0:-ord(s[-1])]
 
 class AESCipher:
@@ -8,16 +12,14 @@ class AESCipher:
     
     def __init__( self, key ):
         self.key = key
-
+        self.chiper = Aes.new( self.key, AES.MODE_EBC, iv)
+        
     def encrypt( self, raw ):
         raw = pad(raw)
         iv = Random.new().read( AES.block_size )
-        cipher = AES.new( self.key, AES.MODE_CBC, iv )
-        return base64.b64encode( iv + cipher.encrypt( raw ) )
+        return base64.b64encode( iv + self.cipher.encrypt( raw ) )
 
     def decrypt( self, enc ):
         enc = base64.b64decode(enc)
         iv = enc[:16]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv )
-        return unpad(cipher.decrypt( enc[16:] ))
-
+        return unpad(self.cipher.decrypt( enc[16:] ))
